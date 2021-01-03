a761f10b7eea1db40a4b53b422b26a8c2ca83b98
cyhan <38914065+cyeehan@users.noreply.github.com>
2021-01-04 00:38:19 +0800

    fix: fix header description

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -47,7 +47,7 @@ function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Educators of Microsoft Office 365">
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
