dd1f2ff73be33a5d3215eb16493e6b960d6f5fbf
cyhan <38914065+cyeehan@users.noreply.github.com>
2021-05-29 01:06:42 +0800

    docs: add a new excel t&t post (irr function)

--- website/sidebars.js
+++ website/sidebars.js
@@ -87,7 +87,8 @@ module.exports = {
       label: "Excel",
       items: [
         "tips-and-tricks/excel/2021-03-01-remove-n-characters-from-field/remove-n-characters-from-field",
-        "tips-and-tricks/excel/2021-04-21-npv-function/npv-function"
+        "tips-and-tricks/excel/2021-04-21-npv-function/npv-function",
+        "tips-and-tricks/excel/2021-04-21-irr-function/irr-function"
       ],
       collapsed: false,
     },
