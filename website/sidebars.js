c340732c237cb7507a35b927fceb75db71b8adf4
cyhan <38914065+cyeehan@users.noreply.github.com>
2021-06-13 00:52:09 +0800

    refactor: implement sidebar for this new post

--- website/sidebars.js
+++ website/sidebars.js
@@ -92,6 +92,7 @@ module.exports = {
         "tips-and-tricks/excel/2021-04-18-create-pivot-charts/create-pivot-charts",
         "tips-and-tricks/excel/2021-04-27-create-pivot-tables/create-pivot-tables",
         "tips-and-tricks/excel/2021-04-19-how-to-use-pmt-and-subtotal/how-to-use-pmt-and-subtotal",
+        "tips-and-tricks/excel/2021-04-19-vertical-lookup/vertical-lookup",
       ],
       collapsed: false,
     },
