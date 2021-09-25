7c8389d11ede34e86b6f774579afabd0680ebfa2
Yee Han <cyh0011215@gmail.com>
2021-09-25 14:26:47 +0800

    chore: migrate items from docs to t&t, and remove empty skeleton pages

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,47 +2,47 @@ module.exports = {
   /**
    * Documentation section
    */
-  docs: [
-    {
-      type: "doc",
-      id: "intro",
-    },
-    /**
-     * Installation guides
-     */
-    {
-      type: "category",
-      label: "Installation",
-      items: ["installation/install-microsoft-office-365"],
-      collapsed: false,
-    },
-    /**
-     * Excitement Days section
-     */
-    {
-      type: "category",
-      label: "Training & Recruitment",
-      items: ["training-and-recruitment-sessions/2021-01-23-24-27-session"],
-      collapsed: false,
-    },
-    /**
-     * Excitement Days section
-     */
-    {
-      type: "category",
-      label: "Excitement Days",
-      items: [],
-    },
-    /**
-     * FAQ section
-     */
-    {
-      type: "category",
-      label: "FAQ",
-      collapsed: false,
-      items: ["faq/faq1"],
-    },
-  ],
+  // docs: [
+  //   {
+  //     type: "doc",
+  //     id: "intro",
+  //   },
+  //   /**
+  //    * Installation guides
+  //    */
+  //   {
+  //     type: "category",
+  //     label: "Installation",
+  //     items: ["installation/install-microsoft-office-365"],
+  //     collapsed: false,
+  //   },
+  //   /**
+  //    * Excitement Days section
+  //    */
+  //   {
+  //     type: "category",
+  //     label: "Training & Recruitment",
+  //     items: ["training-and-recruitment-sessions/2021-01-23-24-27-session"],
+  //     collapsed: false,
+  //   },
+  //   /**
+  //    * Excitement Days section
+  //    */
+  //   {
+  //     type: "category",
+  //     label: "Excitement Days",
+  //     items: [],
+  //   },
+  //   /**
+  //    * FAQ section
+  //    */
+  //   {
+  //     type: "category",
+  //     label: "FAQ",
+  //     collapsed: false,
+  //     items: ["faq/faq1"],
+  //   },
+  // ],
 
   /**
    * FYI: * It is an example of multiple sidebars
@@ -61,15 +61,19 @@ module.exports = {
       id: "tips-and-tricks/introduction",
     },
     {
-      type: "category",
-      label: "Teams",
-      items: ["tips-and-tricks/teams/teams-1"],
-    },
-    {
-      type: "category",
-      label: "PowerPoint",
-      items: ["tips-and-tricks/powerpoint/powerpoint-1"],
-    },
+			type: "doc",
+			id: "tips-and-tricks/installation/install-microsoft-office-365",
+		},
+    // {
+    //   type: "category",
+    //   label: "Teams",
+    //   items: ["tips-and-tricks/teams/teams-1"],
+    // },
+    // {
+    //   type: "category",
+    //   label: "PowerPoint",
+    //   items: ["tips-and-tricks/powerpoint/powerpoint-1"],
+    // },
     {
       type: "category",
       label: "Word",
@@ -98,30 +102,36 @@ module.exports = {
       ],
       collapsed: false,
     },
+    // {
+    //   type: "category",
+    //   label: "Outlook",
+    //   items: ["tips-and-tricks/outlook/outlook-1"],
+    // },
+    // {
+    //   type: "category",
+    //   label: "OneNote",
+    //   items: ["tips-and-tricks/onenote/onenote-1"],
+    // },
+    // {
+    //   type: "category",
+    //   label: "OneDrive",
+    //   items: ["tips-and-tricks/onedrive/onedrive-1"],
+    // },
+    // {
+    //   type: "category",
+    //   label: "Sway",
+    //   items: ["tips-and-tricks/sway/sway-1"],
+    // },
+    // {
+    //   type: "category",
+    //   label: "Power BI",
+    //   items: ["tips-and-tricks/power-bi/power-bi-1"],
+    // },
     {
       type: "category",
-      label: "Outlook",
-      items: ["tips-and-tricks/outlook/outlook-1"],
-    },
-    {
-      type: "category",
-      label: "OneNote",
-      items: ["tips-and-tricks/onenote/onenote-1"],
-    },
-    {
-      type: "category",
-      label: "OneDrive",
-      items: ["tips-and-tricks/onedrive/onedrive-1"],
-    },
-    {
-      type: "category",
-      label: "Sway",
-      items: ["tips-and-tricks/sway/sway-1"],
-    },
-    {
-      type: "category",
-      label: "Power BI",
-      items: ["tips-and-tricks/power-bi/power-bi-1"],
+      label: "FAQ",
+      collapsed: false,
+      items: ["faq/faq1"],
     },
     // End
   ],
