6755f32174ce106b7808b496a677a5b4ed31afc5
Yee Han <cyh0011215@gmail.com>
2021-09-26 20:04:05 +0800

    refactor: clsx for styling

--- website/src/pages/feedback.js
+++ website/src/pages/feedback.js
@@ -1,5 +1,7 @@
 import Layout from '@theme/Layout';
 import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
 
 function Feedback() {
   return (
@@ -8,14 +10,7 @@ function Feedback() {
       description="Feedback form for reinvention and experience improvement"
     >
       <iframe
-        style={{
-          minWidth: '100%',
-          height: '100vh',
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          justifyContent: 'center',
-        }}
+        className={clsx('feedback-form', styles.feedbackForm)}
         src="https://forms.office.com/Pages/ResponsePage.aspx?id=4tZeri9oNkSj0tBxhvLB2iBKZKHFr79KjmxF1C9qLrpUNVNTS1UzVlJaME4wUk43SUpOOUsxTDZaVy4u&embed=true"
         allowfullscreen
         webkitallowfullscreen
