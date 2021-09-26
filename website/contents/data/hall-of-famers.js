6683b7fe400a27e1dd9ab5f5d8b8b287ae51c02d
Yee Han <cyh0011215@gmail.com>
2021-09-26 12:32:41 +0800

    feat: add hall of famers and style layout

---
+++ website/contents/data/hall-of-famers.js
@@ -0,0 +1,32 @@
+export const hallOfFamers = [
+  {
+    name: "Casandra Seng",
+    position: "Business Group Lead - Modern Workplace at Microsoft",
+    linkedin: "https://www.linkedin.com/in/casandra-seng-11797931",
+    image: "/img/hall-of-famers/casandra-seng.jpg",
+  },
+  {
+    name: "Teoh Mun Hong",
+    position: "Adoption Specialist at Microsoft",
+    linkedin: "https://www.linkedin.com/in/teoh-mun-hong-442190bb",
+    image: "/img/hall-of-famers/mun-hong-teoh.jpg",
+  },
+  {
+    name: "Phoenix Yee",
+    position: "Business Development at SoftwareOne",
+		linkedin: "https://www.linkedin.com/in/phoenixyee/",
+		image: "/img/squad-profile-images/phoenix-yee.jpg",
+  },
+  {
+    name: "Jernyn Kah Yee Ho",
+    position: "Analyst at Averis",
+		linkedin: "https://www.linkedin.com/in/jernyn-ho-b1a179141/",
+		image: "/img/squad-profile-images/jernyn-kah-yee-ho.jpg",
+  },
+  {
+		name: "Evelyn Sze Yin Cheah",
+    position: "AVP of Corp. Comm. & Mkt at Malaysian Students'​ Global Alliance (MSGA)",
+		linkedin: "https://www.linkedin.com/in/szeyincheahevelyn/",
+		image: "/img/squad-profile-images/sze-yin-cheah-evelyn.jpg",
+	},
+];
--- website/src/pages/quarter-leaders.js
+++ website/src/pages/quarter-leaders.js
@@ -1,35 +1,53 @@
 import Layout from "@theme/Layout";
 import React, { useState } from "react";
 import { leaders } from "../../contents/data/quarter-leaders";
+import { hallOfFamers } from '../../contents/data/hall-of-famers';
 import styles from "./squad.module.css";
 
 function QuarterLeaders() {
   
   let count;
   const [data, setData] = useState({leaders});
+  const [hallOfFamersData, setHallOfFamer] = useState({hallOfFamers});
   const listPerson = data.leaders.map((data, idx) => {
     const { quarter, name, position, linkedin, image } = data;
     count = 4;
 
     return (
       <>
-        {idx % 4 === 0 ? <><br/><br/><h3>{quarter}</h3></> : ""}
+        {idx % 4 === 0 ? <><h4 className={styles.each_quarter}>{quarter}</h4></> : ""}
         <div className={styles.main} key={idx}>
           <div className={styles.person}>
             <div className={styles.name}> <a href={linkedin}>{name}</a></div>
             <img src={image} alt={name} className={styles.img} />
             <br />
-            <div className={styles.name}>{position}</div>
+            {position? <div className={styles.position}>{position}</div> : ''}
           </div>
         </div>
       </>
     );
   });
 
+  const hallOfFamer = hallOfFamersData.hallOfFamers.map((data, idx) => {
+    return (
+      <div className={styles.main} key={idx}>
+        <div className={styles.person}>
+          <div className={styles.name}> <a href={"linkedin"}>{data.name}</a></div>
+          <img src={data.image} alt={data.name} className={styles.img} />
+          <br />
+          <div className={styles.position}>{data.position}</div>
+        </div>
+      </div>
+    );
+  });
+
+
   return (
     <Layout title="Quarter Leaders" description="Quarter Leaders of Modern Workplace Squad throughout the journey.">
       <div className={styles.container}>
-        <h1>Quarter Leaders</h1>
+        <h1>Hall of Famers</h1>
+        {hallOfFamer}
+        <h1 className={styles.quarter_leader}>Quarter Leaders</h1>
         {listPerson}
       </div>
     </Layout>
