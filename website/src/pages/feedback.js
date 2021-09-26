10a8545760b95fd9d4ec4d9dc94ef050c146535f
Yee Han <cyh0011215@gmail.com>
2021-09-26 19:38:25 +0800

    refactor: add title and description

--- website/src/pages/feedback.js
+++ website/src/pages/feedback.js
@@ -3,24 +3,25 @@ import React from 'react';
 
 function Feedback() {
   return (
-    <Layout>
-      <div>
-        <iframe
-          style={{
-            minWidth: '100%',
-            height: '100vh',
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            justifyContent: 'center',
-          }}
-          src="https://forms.office.com/Pages/ResponsePage.aspx?id=4tZeri9oNkSj0tBxhvLB2iBKZKHFr79KjmxF1C9qLrpUNVNTS1UzVlJaME4wUk43SUpOOUsxTDZaVy4u&embed=true"
-          allowfullscreen
-          webkitallowfullscreen
-          mozallowfullscreen
-          msallowfullscreen
-        />
-      </div>
+    <Layout
+      title="Microsoft Modern Workplace Squad Feedback Form"
+      description="Feedback form for reinvention and experience improvement"
+    >
+      <iframe
+        style={{
+          minWidth: '100%',
+          height: '100vh',
+          display: 'block',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          justifyContent: 'center',
+        }}
+        src="https://forms.office.com/Pages/ResponsePage.aspx?id=4tZeri9oNkSj0tBxhvLB2iBKZKHFr79KjmxF1C9qLrpUNVNTS1UzVlJaME4wUk43SUpOOUsxTDZaVy4u&embed=true"
+        allowfullscreen
+        webkitallowfullscreen
+        mozallowfullscreen
+        msallowfullscreen
+      />
     </Layout>
   );
 }
